2f4eac8d961a076307d26e4269faf95146983342
raymond <raymond.ma02@sap.com>
2018-07-14 11:09:21 +0800

    Add CorsConfig

---
+++ src/main/java/com/aawashcar/apigateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.aawashcar.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	static {
+        System.err.println("initializing CorsConfig...");
+    }
+	
+	private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setMaxAge(18000L);
+        corsConfiguration.setAllowCredentials(true);
+ 
+        corsConfiguration.addAllowedOrigin("*"); // 1
+        corsConfiguration.addAllowedHeader("*"); // 2
+        corsConfiguration.addAllowedMethod("*"); // 3
+        return corsConfiguration;
+    }
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/aawashcar/apigateway/controller/AppController.java
+++ src/main/java/com/aawashcar/apigateway/controller/AppController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import com.aawashcar.apigateway.entity.MiniAuthEntity;
-import com.aawashcar.apigateway.model.MiniAuthModel;
-import com.aawashcar.apigateway.util.EntityMapper;
 
 @RequestMapping("app/")
 @ResponseBody
